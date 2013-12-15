431c89e8e620d59295afc4ee2fd5ea0d1355bff2
Kenji Ono <keno@riken.jp>
2013-12-15 12:27:56 +0900

    r21 check packaging on branch check_on_K [ver 3.1.8]

---
+++ include/Version.h
@@ -0,0 +1,24 @@
+/*
+ * Cutlib - Cut Information Library
+ *
+ * Copyright (c) 2010-2011 VCAD System Research Program, RIKEN.
+ * All rights reserved.
+ *
+ * Copyright (c) 2012-2013 Advanced Institute for Computational Science, RIKEN.
+ * All rights reserved.
+ *
+ */
+
+/**
+ * @file   Version.h
+ * Cutlibバージョン情報のヘッダーファイル
+ */
+
+#ifndef _CUTLIB_VERSION_H_
+#define _CUTLIB_VERSION_H_
+
+#define CT_VERSION_NO  "3.1.8"
+#define CT_REVISION    "20131215_1200"
+
+#endif /* _CUTLIB_VERSION_H_ */
+
