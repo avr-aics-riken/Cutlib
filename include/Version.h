54ed90cdf6593959fae1ac9c0108722cf59f1a87
Kenji Ono <keno@riken.jp>
2014-01-19 17:01:56 +0900

    r22 update year 2014 for copyright [ver 3.2.0]

--- include/Version.h
+++ include/Version.h
@@ -4,7 +4,7 @@
  * Copyright (c) 2010-2011 VCAD System Research Program, RIKEN.
  * All rights reserved.
  *
- * Copyright (c) 2012-2013 Advanced Institute for Computational Science, RIKEN.
+ * Copyright (c) 2012-2014 Advanced Institute for Computational Science, RIKEN.
  * All rights reserved.
  *
  */
@@ -17,8 +17,8 @@
 #ifndef _CUTLIB_VERSION_H_
 #define _CUTLIB_VERSION_H_
 
-#define CT_VERSION_NO  "3.1.8"
-#define CT_REVISION    "20131215_1200"
+#define CT_VERSION_NO  "3.2.0"
+#define CT_REVISION    "20140119_1700"
 
 #endif /* _CUTLIB_VERSION_H_ */
 
